8282bf6fad147c1d6af0a1f0c7ec936e5e067d3c
eallyy <eallyy@icloud.com>
2022-04-01 03:08:40 +0300

    main branch edited

--- setup.sh
+++
@@ -1,9 +0,0 @@
-apt-get update
-apt-get upgrade -y
-apt-get install apache2 -y
-apt-get install ufw -y
-ufw allow http
-ufw allow https
-apt-get install mysql-server -y
-apt-get install php libapache2-mod-php php-mcrypt php-mysql php-cgi php-curl php-json
-echo "<?= phpinfo() ?>" >> /var/www/html/index.php
